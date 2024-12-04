034cbb3b5287102d26e4fb61af64a8a031e85795
mitra <mitra@Aditya-Mitra.local>
2024-12-04 17:08:12 -0500

    Backend changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const appointmentRoutes = require('./routes/appointments');
-const forumRoutes = require('./routes/forum');
+
 
 dotenv.config();
 const app = express();
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/appointments', appointmentRoutes);
-app.use('/api/forum', forumRoutes);
+
 
 mongoose.connect('mongodb://localhost:27017/healthcare', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
